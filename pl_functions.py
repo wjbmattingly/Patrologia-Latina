10983041f181fba5cea09bd63a3721003cd7c016
wjbmattingly <62964060+wjbmattingly@users.noreply.github.com>
2020-07-07 16:36:07 -0400

    Create pl_functions.py

---
+++ pl_functions.py
@@ -0,0 +1,21 @@
+import re
+from string import digits
+
+#Feed a string of an entire PL text and this function will output a tuple of a cleaned text (no punctuation) and a lowercased (everything lowercased). The result is data cleaned and prepped for machine learning.
+def clean_pl_ml(text):
+        cleaned = re.sub(r"[\(\[].*?[\)\]]", "", text).replace(" ,", ",").replace(" .", ".").replace(" .", ".").replace("\n\n", "\n").replace("\n\n", "\n").replace("\n\n", "\n").replace("  ", " ").replace("  ", " ").replace("\n ", "\n").replace(" ;", ";").replace(" :", ":").replace(" ?", "?").replace("?  ", "? ").replace(" ,", ",").replace("J", "I").replace("j", "i")
+        remove_digits = str.maketrans('', '', digits)
+        cleaned = cleaned.translate(remove_digits)
+
+        with open (new_filename, "w", encoding="utf-8") as new_f:
+            new_f.write(cleaned)
+        with open (lowered, "w", encoding="utf-8") as new_f:
+            l_data = cleaned.lower()
+            punctuations = '''!()-[]{};:'"\,<>./?@#$%^&*_~'''
+            lp_data = ""
+            for char in l_data:
+                if char not in punctuations:
+                    lp_data = lp_data+char
+            lowercase_data = lp_data.replace("\n", " ")
+            new_f.write(lowercase_data)
+        return (cleaned, lowercase_data)
